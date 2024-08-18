707d81e04aabf427d767f96c107dfc8a94755adc
Peiwast Hama <peiwast124@gmail..com>
2024-08-18 15:27:47 +0200

    Legge til proxyserver

--- frontend/src/Step1.js
+++ frontend/src/Step1.js
@@ -22,40 +22,52 @@ const Step1 = ({ formData, setFormData, nextStep }) => {
         onSubmit={async (values) => {
           try {
             const response = await fetch(`https://rimelig-auksjon-backend.vercel.app/proxy/api/carinfo/${values.regNumber}`);
-            const carData = await response.json();
-            const carInfo = carData.kjoretoydataListe ? carData.kjoretoydataListe[0] : {};
-            const tekniskeData = carInfo.godkjenning?.tekniskGodkjenning?.tekniskeData || {};
-
-            const updatedFormData = {
-              ...formData,
-              ...values,
-              brand: tekniskeData.generelt?.merke[0]?.merke || '',
-              model: tekniskeData.generelt?.handelsbetegnelse[0] || '',
-              year: carInfo.godkjenning?.forstegangsGodkjenning?.forstegangRegistrertDato?.split('-')[0] || '',
-              chassisNumber: carInfo.kjoretoyId?.understellsnummer || '',
-              taxClass: carInfo.godkjenning?.tekniskGodkjenning?.kjoretoyklassifisering?.beskrivelse || '',
-              fuel: tekniskeData.miljodata?.miljoOgdrivstoffGruppe ? tekniskeData.miljodata.miljoOgdrivstoffGruppe[0]?.drivstoffKodeMiljodata?.kodeNavn || '' : '',
-              gearType: tekniskeData.motorOgDrivverk?.girkassetype?.kodeBeskrivelse || '',
-              driveType: tekniskeData.motorOgDrivverk?.kjoresystem?.kodeBeskrivelse || '',
-              mainColor: tekniskeData.karosseriOgLasteplan?.rFarge ? tekniskeData.karosseriOgLasteplan.rFarge[0]?.kodeNavn || '' : '',
-              power: tekniskeData.motorOgDrivverk?.motor && tekniskeData.motorOgDrivverk.motor.length > 0 ? tekniskeData.motorOgDrivverk.motor[0]?.maksNettoEffekt || '' : '',
-              seats: tekniskeData.persontall?.sitteplasserTotalt || '',
-              owners: carInfo.eierskap?.antall || '',
-              firstRegistration: carInfo.godkjenning?.forstegangsGodkjenning?.forstegangRegistrertDato || '',
-              doors: tekniskeData.karosseriOgLasteplan?.antallDorer ? tekniskeData.karosseriOgLasteplan.antallDorer[0] || '' : '',
-              weight: tekniskeData.vekter?.egenvekt || '',
-              co2: tekniskeData.miljodata?.forbrukOgUtslipp?.length > 0 ? tekniskeData.miljodata.forbrukOgUtslipp[0]?.co2BlandetKjoring || '' : '',
-              omregistreringsavgift: carInfo.omregistreringsavgift || '',
-              lastEUApproval: carInfo.periodiskKjoretoyKontroll?.sistGodkjent || '',
-              nextEUControl: carInfo.periodiskKjoretoyKontroll?.kontrollfrist || '',
-            };
-
-            setFormData(updatedFormData);
-            nextStep();
+        
+            // Check the content type of the response
+            const contentType = response.headers.get('content-type');
+            if (contentType && contentType.indexOf('application/json') !== -1) {
+              // Parse JSON if content type is application/json
+              const carData = await response.json();
+              const carInfo = carData.kjoretoydataListe ? carData.kjoretoydataListe[0] : {};
+              const tekniskeData = carInfo.godkjenning?.tekniskGodkjenning?.tekniskeData || {};
+        
+              const updatedFormData = {
+                ...formData,
+                ...values,
+                brand: tekniskeData.generelt?.merke[0]?.merke || '',
+                model: tekniskeData.generelt?.handelsbetegnelse[0] || '',
+                year: carInfo.godkjenning?.forstegangsGodkjenning?.forstegangRegistrertDato?.split('-')[0] || '',
+                chassisNumber: carInfo.kjoretoyId?.understellsnummer || '',
+                taxClass: carInfo.godkjenning?.tekniskGodkjenning?.kjoretoyklassifisering?.beskrivelse || '',
+                fuel: tekniskeData.miljodata?.miljoOgdrivstoffGruppe ? tekniskeData.miljodata.miljoOgdrivstoffGruppe[0]?.drivstoffKodeMiljodata?.kodeNavn || '' : '',
+                gearType: tekniskeData.motorOgDrivverk?.girkassetype?.kodeBeskrivelse || '',
+                driveType: tekniskeData.motorOgDrivverk?.kjoresystem?.kodeBeskrivelse || '',
+                mainColor: tekniskeData.karosseriOgLasteplan?.rFarge ? tekniskeData.karosseriOgLasteplan.rFarge[0]?.kodeNavn || '' : '',
+                power: tekniskeData.motorOgDrivverk?.motor && tekniskeData.motorOgDrivverk.motor.length > 0 ? tekniskeData.motorOgDrivverk.motor[0]?.maksNettoEffekt || '' : '',
+                seats: tekniskeData.persontall?.sitteplasserTotalt || '',
+                owners: carInfo.eierskap?.antall || '',
+                firstRegistration: carInfo.godkjenning?.forstegangsGodkjenning?.forstegangRegistrertDato || '',
+                doors: tekniskeData.karosseriOgLasteplan?.antallDorer ? tekniskeData.karosseriOgLasteplan.antallDorer[0] || '' : '',
+                weight: tekniskeData.vekter?.egenvekt || '',
+                co2: tekniskeData.miljodata?.forbrukOgUtslipp?.length > 0 ? tekniskeData.miljodata.forbrukOgUtslipp[0]?.co2BlandetKjoring || '' : '',
+                omregistreringsavgift: carInfo.omregistreringsavgift || '',
+                lastEUApproval: carInfo.periodiskKjoretoyKontroll?.sistGodkjent || '',
+                nextEUControl: carInfo.periodiskKjoretoyKontroll?.kontrollfrist || '',
+              };
+        
+              setFormData(updatedFormData);
+              nextStep();
+            } else {
+              // Handle non-JSON response
+              const textData = await response.text();
+              console.error('Unexpected content type:', contentType);
+              console.error('Response text:', textData);
+            }
           } catch (error) {
             console.error('Error fetching car data:', error);
           }
         }}
+        
       >
         <Form className="bil-form">
           <h2>Registreringsnummer</h2>
