eb197eacd7e0c6a139a738eb0c735a43bcea7509
Peiwast Hama <peiwast124@gmail..com>
2024-08-19 11:26:20 +0200

    Prøve fikse login

--- backend/proxyServer.mjs
+++ backend/proxyServer.mjs
@@ -1,21 +1,27 @@
-// proxyServer.mjs
-
 import express from 'express';
-import fetch from 'node-fetch';
 import cors from 'cors';
 
+// Opprett Express-appen
 const app = express();
-const port = 8081; // Bruk en annen port for proxy-serveren
 
-app.use(cors());
+// CORS-konfigurasjon
+const corsOptions = {
+  origin: 'https://www.rimeligauksjon.no', // Tillat forespørsler fra dette domenet
+  methods: 'GET, POST, PUT, DELETE, OPTIONS', // Tillatte metoder
+  allowedHeaders: 'Content-Type, Authorization', // Tillatte headere
+  credentials: true, // Tillat å sende cookies og auth-info
+};
+
+// Bruk CORS med spesifikke innstillinger
+app.use(cors(corsOptions));
 app.use(express.json());
 
-const API_KEY = 'e59b5fa7-0331-4359-9c99-bbe1a520db87';
+const API_KEY = process.env.SVV_API_KEY;
 
 app.get('/api/carinfo/:regNumber', async (req, res) => {
   const regNumber = req.params.regNumber;
   const url = `http://www.vegvesen.no/ws/no/vegvesen/kjoretoy/felles/datautlevering/enkeltoppslag/kjoretoydata?kjennemerke=${regNumber}`;
-  
+
   try {
     const response = await fetch(url, {
       method: 'GET',
@@ -29,6 +35,13 @@ app.get('/api/carinfo/:regNumber', async (req, res) => {
     }
 
     const data = await response.json();
+
+    // Legg til CORS-headere i responsen
+    res.setHeader('Access-Control-Allow-Origin', 'https://www.rimeligauksjon.no');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+
     res.json(data);
   } catch (error) {
     console.error('Error fetching car data:', error);
@@ -36,6 +49,5 @@ app.get('/api/carinfo/:regNumber', async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Proxy server is running on http://localhost:${port}`);
-});
+// Eksporter appen som standard for Vercel å håndtere
+export default app;
