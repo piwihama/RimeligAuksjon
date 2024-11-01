3c3591ae95aa07f6e220c43316ce154b55dc3ead
piwihama <peiwast124@gmail.com>
2024-11-01 11:11:52 +0100

    Fikse bug

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -98,22 +98,20 @@ function Header({ onCategorySelect }) {
             )}
           </div>
         </div>
-
         <nav className="menu">
-        <button onClick={() => onCategorySelect('car')} className="menu-button">
-  <i className="material-icons">directions_car</i>Bil
-</button>
-<button onClick={() => onCategorySelect('boat')} className="menu-button">
-  <i className="material-icons">directions_boat</i>Båt
-</button>
-<button onClick={() => onCategorySelect('motorcycle')} className="menu-button">
-  <i className="material-icons">two_wheeler</i>MC
-</button>
-<button onClick={() => onCategorySelect('marketplace')} className="menu-button">
-  <i className="material-icons">store</i>Torg
-</button>
-
-        </nav>
+  <button onClick={() => onCategorySelect && onCategorySelect('car')} className="menu-button">
+    <i className="material-icons">directions_car</i>Bil
+  </button>
+  <button onClick={() => onCategorySelect && onCategorySelect('boat')} className="menu-button">
+    <i className="material-icons">directions_boat</i>Båt
+  </button>
+  <button onClick={() => onCategorySelect && onCategorySelect('motorcycle')} className="menu-button">
+    <i className="material-icons">two_wheeler</i>MC
+  </button>
+  <button onClick={() => onCategorySelect && onCategorySelect('marketplace')} className="menu-button">
+    <i className="material-icons">store</i>Torg
+  </button>
+</nav>
 
         <LoginModal isOpen={modalOpen} onRequestClose={() => setModalOpen(false)} purpose={modalPurpose} />
       </header>
