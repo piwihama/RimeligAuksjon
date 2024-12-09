cd0e3a907d2f95d344a5c4f556e6d0eb869d7a11
piwihama <peiwast124@gmail.com>
2024-12-09 13:20:43 +0100

    Prøve drag and drop step4

--- frontend/src/Step4.js
+++ frontend/src/Step4.js
@@ -22,19 +22,16 @@ const Step4 = ({ formData, setFormData, nextStep, prevStep }) => {
         animation: 150,
         onEnd: (evt) => {
           const newOrder = [...previewImages];
-          const [movedItem] = newOrder.splice(evt.oldIndex, 1);
-          newOrder.splice(evt.newIndex, 0, movedItem);
-  
-          // Filtrer ut eventuelle undefined elementer
-          const filteredOrder = newOrder.filter((item) => item !== undefined);
-  
-          setPreviewImages(filteredOrder);
-          setFormData({ ...formData, previewImages: filteredOrder });
+          if (evt.oldIndex !== undefined && evt.newIndex !== undefined) {
+            const [movedItem] = newOrder.splice(evt.oldIndex, 1);
+            newOrder.splice(evt.newIndex, 0, movedItem);
+            setPreviewImages(newOrder);
+            setFormData({ ...formData, previewImages: newOrder });
+          }
         },
       });
     }
   }, [previewImages, setFormData]);
-  
 
   const handleImageUpload = async (event, setFieldValue) => {
     const files = Array.from(event.target.files);
