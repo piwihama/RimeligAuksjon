9ff052a6706ea2747ed529e5d73119c831007ea8
piwihama <peiwast124@gmail.com>
2024-12-15 14:40:32 +0100

    Style home

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -9,36 +9,65 @@ import Footer from './Footer';  // Juster stien hvis Footeren ligger et annet st
 function Home() {
   const [auctions, setAuctions] = useState([]);
   const [visibleAuctions, setVisibleAuctions] = useState([]);
-  const [currentIndex, setCurrentIndex] = useState(0);
   const [loading, setLoading] = useState(true); // Legger til loading state
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetchAuctions();
   }, []);
-  
-  const navigate = useNavigate();
 
-  const handleButtonClick = () => {
-    navigate('/info');//
+  const fetchAuctions = async () => {
+    try {
+      const response = await axios.get('https://rimelig-auksjon-backend.vercel.app/api/liveauctions');
+      const auctionsWithTimeLeft = response.data.map((auction) => ({
+        ...auction,
+        timeLeft: calculateTimeLeft(auction.endDate),
+      }));
+      setAuctions(auctionsWithTimeLeft);
+      setInitialVisibleAuctions(auctionsWithTimeLeft);
+    } catch (error) {
+      console.error('Error fetching auctions:', error);
+    } finally {
+      setLoading(false); // Sett loading til false når dataene er hentet eller hvis det er en feil
+    }
+  };
+
+  const setInitialVisibleAuctions = (auctionsList) => {
+    if (auctionsList.length >= 4) {
+      setVisibleAuctions(getRandomItems(auctionsList, 4));
+    } else {
+      setVisibleAuctions(auctionsList);
+    }
+  };
+
+  const getRandomItems = (list, count) => {
+    const shuffled = [...list].sort(() => 0.5 - Math.random());
+    return shuffled.slice(0, count);
   };
 
   useEffect(() => {
-    if (auctions.length > 0) {
-      setVisibleAuctions(auctions.slice(0, 3));
-    }
-  }, [auctions]);
-
-  useEffect(() => {
     const interval = setInterval(() => {
-      if (auctions.length > 0) {
-        const nextIndex = (currentIndex + 1) % auctions.length;
-        setCurrentIndex(nextIndex);
-        setVisibleAuctions(auctions.slice(nextIndex, nextIndex + 4));
+      if (auctions.length > 4) {
+        setVisibleAuctions((prevVisible) => {
+          const remainingAuctions = auctions.filter(
+            (auction) => !prevVisible.includes(auction)
+          );
+
+          if (remainingAuctions.length === 0) return prevVisible;
+
+          const randomNewAuction = remainingAuctions[Math.floor(Math.random() * remainingAuctions.length)];
+          const randomIndexToReplace = Math.floor(Math.random() * prevVisible.length);
+
+          const newVisible = [...prevVisible];
+          newVisible[randomIndexToReplace] = randomNewAuction;
+
+          return newVisible;
+        });
       }
-    }, 5000); // Change auction every 5 seconds
+    }, 5000); // Bytt ut én auksjon hvert 5. sekund
 
     return () => clearInterval(interval);
-  }, [auctions, currentIndex]);
+  }, [auctions]);
 
   const calculateTimeLeft = (endDate) => {
     const difference = new Date(endDate) - new Date();
@@ -63,90 +92,65 @@ function Home() {
     return timeLeft;
   };
 
-  const updateAuctionTimes = () => {
-    setVisibleAuctions((prevAuctions) =>
-      prevAuctions.map((auction) => ({
-        ...auction,
-        timeLeft: calculateTimeLeft(auction.endDate),
-      }))
-    );
-  };
-
-  useEffect(() => {
-    const interval = setInterval(updateAuctionTimes, 1000); // Update every second
-
-    return () => clearInterval(interval);
-  }, [visibleAuctions]);
-
-  const fetchAuctions = async () => {
-    try {
-      const response = await axios.get('https://rimelig-auksjon-backend.vercel.app/api/liveauctions');
-      console.log(response.data);  // Legg til denne linjen for å se hele responsen
-
-      const auctionsWithTimeLeft = response.data.map((auction) => ({
-        ...auction,
-        timeLeft: calculateTimeLeft(auction.endDate),
-      }));
-      setAuctions(auctionsWithTimeLeft);
-    } catch (error) {
-      console.error('Error fetching auctions:', error);
-    } finally {
-      setLoading(false); // Sett loading til false når dataene er hentet eller hvis det er en feil
-    }
-  };
-
   return (
     <div>
       <Header />
       <div className="home-container">
         <div className="home-content">
-        <div className="home-banner">
-          <div className="home-banner-content">
-            <h1>Lavere gebyrer. Bedre auksjonsopplevelse.</h1>
-            <p>Våre auksjonsrådgivere støtter deg gjennom hele salgsprosessen.</p>
-            <strong><p>Helt kostnadsfritt</p></strong>
-            <button className="home-banner-button" onClick={handleButtonClick}>
-              Les mer her
-            </button>
+          <div className="home-banner">
+            <div className="home-banner-content">
+              <h1>Lavere gebyrer. Bedre auksjonsopplevelse.</h1>
+              <p>Våre auksjonsrådgivere støtter deg gjennom hele salgsprosessen.</p>
+              <strong><p>Helt kostnadsfritt</p></strong>
+              <button className="home-banner-button" onClick={() => navigate('/info')}>
+                Les mer her
+              </button>
+            </div>
           </div>
-        </div>
-
 
           <div className="home-auctions-section">
             <h2>Fremhevede Auksjoner</h2>
-            {loading ? ( // Hvis loading er true, vis lastemeldingen
+            {loading ? (
               <p>Laster inn auksjoner...</p>
-            ) : auctions.length === 0 ? ( // Hvis det ikke er noen auksjoner
+            ) : auctions.length === 0 ? (
               <p>Ingen auksjoner tilgjengelig for øyeblikket.</p>
             ) : (
               <div className="home-auction-list">
                 {visibleAuctions.map((auction) => (
-                  <div key={auction._id} className="home-auction-item" onClick={() => navigate(`/liveauctions/${auction._id}`)}
-                    style={{ cursor: 'pointer' }} >
-                    <img 
+                  <div
+                    key={auction._id}
+                    className="home-auction-item"
+                    onClick={() => navigate(`/liveauctions/${auction._id}`)}
+                    style={{ cursor: 'pointer' }}
+                  >
+                    <img
                       src={auction.imageUrls && auction.imageUrls.length > 0 ? auction.imageUrls[0] : '/path-to-default-image.jpg'}
-                      alt={`${auction.brand} ${auction.model}`} 
-                      className="home-auction-image" 
+                      alt={`${auction.brand} ${auction.model}`}
+                      className="home-auction-image"
                     />
                     <div className="home-auction-details">
-                      <h3>{auction.brand} {auction.model} {auction.year} </h3>
+                      <h3>
+                        {auction.brand} {auction.model} {auction.year}
+                      </h3>
                       <span>{auction.mileage} KM</span>
-
-                      <div className='home-auction-smalldetails'>
-                        <div className='home-title-value-auction'>
-                          <span className="home-auction-title"><strong>Gjenstår:</strong></span>
+                      <div className="home-auction-smalldetails">
+                        <div className="home-title-value-auction">
+                          <span className="home-auction-title">
+                            <strong>Gjenstår:</strong>
+                          </span>
                           <span className="home-auction-value" style={{ color: 'rgb(211, 13, 13)', fontWeight: 'bold' }}>
                             {auction.timeLeft.days} Dager {auction.timeLeft.hours}t {auction.timeLeft.minutes}min {auction.timeLeft.seconds}sek
                           </span>
                         </div>
-                        <div className='home-title-value-auction'>
-                          <span className="home-auction-title"><strong>Høyeste Bud:</strong></span>
-                          <span className="home-auction-value" style={{ color: 'rgb(211, 13, 13)', fontWeight: 'bold' }}>{auction.highestBid},-</span>
+                        <div className="home-title-value-auction">
+                          <span className="home-auction-title">
+                            <strong>Høyeste Bud:</strong>
+                          </span>
+                          <span className="home-auction-value" style={{ color: 'rgb(211, 13, 13)', fontWeight: 'bold' }}>
+                            {auction.highestBid},-
+                          </span>
                         </div>
                       </div>
-                      
-                    
-                      
                     </div>
                   </div>
                 ))}
